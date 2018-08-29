74770a0a8642f60095babdc53fc4187a3cb0fd20
Nelson Alejandro Hidalgo <nahidalgo@yahoo.com.br>
2018-08-28 22:01:56 -0300

    Initial commit

---
+++ Assets/Scripts/BoxClick.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxClick : MonoBehaviour {
+
+	public Button btn;
+	public Image buttonImage;
+	private bool selected = false;
+	//private GameLogic logic;
+	[SerializeField] private SpriteRenderer spriteR;
+	private Sprite[] sprites;
+
+	public void SetNumber(){
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			selected = false;
+			spriteR.sprite = sprites[0];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha2)){
+			selected = false;
+			spriteR.sprite = sprites[1];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha3)){
+			selected = false;
+			spriteR.sprite = sprites[2];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha4)){
+			selected = false;
+			spriteR.sprite = sprites[3];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha5)){
+			selected = false;
+			spriteR.sprite = sprites[4];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha6)){
+			selected = false;
+			spriteR.sprite = sprites[5];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha7)){
+			selected = false;
+			spriteR.sprite = sprites[6];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha8)){
+			selected = false;
+			spriteR.sprite = sprites[7];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha9)){
+			selected = false;
+			spriteR.sprite = sprites[8];
+			//logic.CheckForEquals(buttonImage.sprite.ToString());
+		}
+		
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		btn = GetComponent<Button>();
+		buttonImage = GetComponent<Image>();
+		spriteR = gameObject.GetComponent<SpriteRenderer>();
+		Resources.LoadAll<Sprite>("numbers-sprite-100");
+		
+		btn.onClick.AddListener(Select);
+	}
+
+	private void Update() {
+		if(selected)
+			SetNumber();
+	}
+
+	private void Select(){ //faz com que o botao seja selecionado e aguarde input
+		selected = !selected;
+	}
+}
---
+++ Assets/Scripts/NumberValue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// ESTE SCRIPT NAO ESTA SENDO UTILIZADO NO MOMENTO, O OBJETIVO DELE EH MUDAR A SPRITE DA CAIXA
+// NO ENTANTO ESSA FUNCIONALIDADE FOI MOVIDA SEM SUCESSO PARA A BOXCLICK
+public class NumberValue : MonoBehaviour {
+
+	private int number;
+	
+	private SpriteRenderer spriteR;
+	private Sprite[] sprites;
+
+	private void Start() {
+		spriteR = gameObject.GetComponent<SpriteRenderer>();
+		sprites = Resources.LoadAll<Sprite>("numbers-sprite-100");
+	}
+	public void SpriteAssigner(int number, Image buttonImage){
+		if(number == 1)
+			buttonImage.sprite = sprites[0];
+		if(number == 2)
+			buttonImage.sprite = sprites[1];
+		if(number == 3)
+			buttonImage.sprite = sprites[2];
+		if(number == 4)
+			buttonImage.sprite = sprites[3];
+		if(number == 5)
+			buttonImage.sprite = sprites[4];
+		if(number == 6)
+			buttonImage.sprite = sprites[5];
+		if(number == 7)
+			buttonImage.sprite = sprites[6];
+		if(number == 8)
+			buttonImage.sprite = sprites[7];
+		if(number == 9)
+			buttonImage.sprite = sprites[8];
+
+	}
+}
